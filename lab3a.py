8ff9d20bcf3efe4ca78ebecf4211ee140b9251fb
Bikesh Shrestha <bshrestha24@myseneca.ca>
2024-05-30 15:18:20 -0400

    Added lab3a

---
+++ lab3a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Author ID: 160637229
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+    return 15
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
